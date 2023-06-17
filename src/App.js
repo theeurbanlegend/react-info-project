fdf42a956b1dd29765f2461376dee3d1924c716d
theeurbanlegend <mbuto152@gmail.com>
2023-06-17 14:14:46 +0300

    First React page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-  return (
-    <div>
+  const content=(
+    
+    <div className='App-header'>
+      <img src="./logo192.png"  className="App-logo"/>
+      <h1 > Fun Facts about React</h1>
       <ul>
-        <li> My first react modification</li>
-
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars in Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps,including mobile apps</li>
       </ul>
     </div>
-  );
+  )
+  return content
+ 
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+// React 18 new update
+// ReactDOM.render is not supported
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+// React 18 new update
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
