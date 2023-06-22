c7027285b1ba309e50af514ec6fe9148df7092b5
theeurbanlegend <mbuto152@gmail.com>
2023-06-22 10:58:29 +0300

    Further Styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 
 function App(){
   return(
-    <div className='App-header '>
+    <div className='App-Body'>
     <AppHeader />
     <Body />
     <Footer/>
--- src/AppHeader.js
+++ src/AppHeader.js
@@ -4,10 +4,8 @@ function AppHeader(){
         <div className="App-nav">
           <div className='React' >
             <img src="logo192.png" className="App-logo"/>
-            <p>ReactFacts</p>
-          </div>
-          <div >
-            <p>React Course-Project 1</p>
+            <h3>ReactFacts</h3>
+            <h4>React Course-Project 1</h4>
           </div>
         </div>
         <h1> Fun Facts about React</h1>
