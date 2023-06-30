75b3d7cfb71952cc206a0d5fefbe6260f02fd7df
theeurbanlegend <mbuto152@gmail.com>
2023-06-30 18:45:56 +0300

    Adding some

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -1,11 +1,26 @@
+import React from "react"
+import './App.css'
 function AppHeader(){
+  let Mode=React.useState(false)
+   function ToggleMode(){
+    console.log(Mode)
+  }
     return (
       <div>
         <div className="App-nav">
           <div className='React' >
             <img src="logo192.png" className="App-logo"/>
+            <div>
             <h3>ReactFacts</h3>
-            <h4>React Course-Project 1</h4>
+            </div>
+            <div onClick={ToggleMode}className="ToggleButton">
+              <h4>Dark</h4>
+              <label className="switch">
+                <input type="checkbox"/>
+                <span className="slider round"></span>
+              </label>
+              <h4>Light</h4>
+            </div>
           </div>
         </div>
       </div>
